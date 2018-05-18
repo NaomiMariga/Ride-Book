30b5f37bb33de66d5db6258adff7c6260046c831
Naomi Mariga <kaburamariga@gmail.com>
2018-05-18 23:41:41 +0300

    add the database models and class files

---
+++ db_conn.php
@@ -0,0 +1,8 @@
+<?php
+// define database connection
+define('SERVER','localhost:3036);
+define('USER', 'root');
+define('PASSWORD', '1234root');
+$connection = mysqli_connect(SERVER,USER, PASSWORD);
+
+?>
